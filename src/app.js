b72d7561bb845c060ba32859f5f0a23b10749a04
Gujeah <bme-011-17@must.ac.mw>
2024-04-03 16:43:59 +0200

    Modified weather data and javascript

--- src/app.js
+++ src/app.js
@@ -4,8 +4,39 @@ function refreshweather(response) {
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  //   console.log(response.data.temperature.humidity);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let windElement = document.querySelector("#wind");
+  //   console.log(response.data.wind.speed);
+  windElement.innerHTML = response.data.wind.speed;
+  let timeElement = document.querySelector("#time");
+  console.log(response.data.time);
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  if (minute < 5) {
+    minutes = `0${minute}`;
+  }
+  let day = days[date.getDay()];
+  return `${day}  ${hour}:${minute}`;
+}
 function searchCity(city) {
   //make API call and update everything
   let apiKey = "cf47e9o04a9a746ea196tf8d38bb1156";
