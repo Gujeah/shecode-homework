0927329675c388e68b6a75738d0dd28b6bad8380
Gujeah <bme-011-17@must.ac.mw>
2024-04-03 13:37:00 +0200

    Added search functionalitiy using API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
+function refreshweather(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  //   console.log(response.data.temperature.current);
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  //make API call and update everything
+  let apiKey = "cf47e9o04a9a746ea196tf8d38bb1156";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshweather);
+}
+
 let searchForm = document.querySelector("#search-form");
-console.log(searchForm);
 
 function handleSearch(event) {
   event.preventDefault();
-  let searchinput = document.querySelector("#search");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchinput.value;
+  let searchInput = document.querySelector("#search");
+
+  searchCity(searchInput.value);
 }
 searchForm.addEventListener("submit", handleSearch);
+searchCity("Lisbon");
